bf1083a7e397cc2a75322782b17c717e5c64231f
RomanZhirnov <rvzh@mail.ru>
2022-10-20 18:23:37 +0300

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _movingSpeed = 10;
+
+    private Rigidbody2D _rb2d;
+    private Vector2 _targetPosition;
+
+    void Start()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _targetPosition.x = Input.GetAxisRaw("Horizontal");
+        _targetPosition.y = Input.GetAxisRaw("Vertical");
+
+        _targetPosition = _rb2d.position + _targetPosition;
+
+        _rb2d.position = Vector2.MoveTowards(_rb2d.position, _targetPosition, _movingSpeed*Time.deltaTime);
+    }
+
+}
