aa692b1fb07d2415ea2fd7439712d94cd6c73ae5
RomanZhirnov <rvzh@mail.ru>
2022-10-21 15:04:31 +0300

    AddStopMovingAfterCollision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,22 +8,41 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _movingSpeed = 10;
 
+    private bool _canMove;
     private Rigidbody2D _rb2d;
     private Vector2 _targetPosition;
 
+    public Player _collide;
+
+    private void OnEnable()
+    {
+        _collide.Collide += StopMoving;
+    }
+
+    private void OnDisable()
+    {
+        _collide.Collide -= StopMoving;
+    }
+
     void Start()
     {
+        _canMove = true;
         _rb2d = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        _targetPosition.x = Input.GetAxisRaw("Horizontal");
-        _targetPosition.y = Input.GetAxisRaw("Vertical");
-
-        _targetPosition = _rb2d.position + _targetPosition;
-
-        _rb2d.position =  Vector2.MoveTowards(_rb2d.position, _targetPosition, _movingSpeed*Time.deltaTime);
+        if (_canMove)
+        {
+            _targetPosition.x = Input.GetAxisRaw("Horizontal");
+            _targetPosition.y = Input.GetAxisRaw("Vertical");
+            _targetPosition = _rb2d.position + _targetPosition;
+            _rb2d.position = Vector2.MoveTowards(_rb2d.position, _targetPosition, _movingSpeed * Time.deltaTime);
+        }
     }
 
+    private void StopMoving()
+    {
+            _canMove = false;
+    }
 }
